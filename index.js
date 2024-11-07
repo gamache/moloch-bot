c8318a564c437f4d877acb6d042387cf63ddcc2c
pete gamache <pete@gamache.org>
2024-11-07 09:20:35 -0500

    They broke their backs lifting Moloch to Heaven!

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { createRestAPIClient } from "masto";
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
 const INTERVAL = 30 * 60 * 1000; // 30 minutes
-const MOLOCHS = readFileSync("molochs.txt").toString().trim().split("\n");
+const MOLOCHS = readFileSync("README.txt").toString().trim().split("\n");
 
 let index = 0;
 
@@ -15,9 +15,7 @@ async function post() {
   const moloch = MOLOCHS[index];
   index = (index + 1) % MOLOCHS.length;
 
-  const status = await masto.v1.statuses.create({
-    status: moloch,
-  });
+  const status = await masto.v1.statuses.create({ status: moloch });
 
   console.log(status.url);
   console.log(moloch);
@@ -30,5 +28,5 @@ async function postAndSchedule() {
 
 postAndSchedule();
 
-await new Promise(() => 1312); // wait forever
+await new Promise(() => "wait forever");
 
